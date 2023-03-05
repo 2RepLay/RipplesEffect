16b1e5fb62581f20995d9b0ee40a8b96f600ed38
Nikita Yankov <evne.yankovnikita@gmail.com>
2023-03-05 14:47:25 +0200

    Initial Commit

---
+++ Ripples Effect/Ripples_EffectApp.swift
@@ -0,0 +1,17 @@
+//
+//  Ripples_EffectApp.swift
+//  Ripples Effect
+//
+//  Created by nikita on 05.03.2023.
+//
+
+import SwiftUI
+
+@main
+struct Ripples_EffectApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
